0ac2b745b4b8b8137a340fa24c76b89af1573bc5
soso <dannykulikov123@gmail.com>
2024-11-02 18:57:27 +0300

    Update database

--- database.sql
+++ database.sql
@@ -1,38 +1,38 @@
 -- Таблица курсов
 CREATE TABLE courses (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name_course VARCHAR(255) NOT NULL,
-    description_course TEXT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    is_deleted SMALLINT NOT NULL
+    deleted_at SMALLINT NOT NULL
 );
 
 -- Таблица уроков
 CREATE TABLE lessons (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     course_id BIGINT REFERENCES courses (id) NOT NULL,
-    name_lesson VARCHAR(255) NOT NULL,
-    content_lesson TEXT NOT NULL,
-    link_video VARCHAR(255),
+    name VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    video_url VARCHAR(255),
     position INT NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    is_deleted SMALLINT NOT NULL
+    deleted_at SMALLINT NOT NULL
 );
 
 -- Таблица модулей
 CREATE TABLE modules (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name_module VARCHAR(255) NOT NULL,
-    description_module TEXT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    is_deleted SMALLINT NOT NULL
+    deleted_at SMALLINT NOT NULL
 );
 
 -- связующая таблица курсов и модулей
-CREATE TABLE courses_modules (
+CREATE TABLE course_modules (
     module_id BIGINT REFERENCES modules (id) NOT NULL,
     course_id BIGINT REFERENCES courses (id) NOT NULL,
     PRIMARY KEY (module_id, course_id)
@@ -41,15 +41,15 @@ CREATE TABLE courses_modules (
 -- Таблица программ
 CREATE TABLE programs (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name_program VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
     price DECIMAL(12, 2) NOT NULL,
-    type_program VARCHAR(255) NOT NULL,
+    program_type VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
 
 -- связующая таблица программ и модулей
-CREATE TABLE programs_modules (
+CREATE TABLE program_modules (
     program_id BIGINT REFERENCES programs (id) NOT NULL,
     module_id BIGINT REFERENCES modules (id) NOT NULL,
     PRIMARY KEY (program_id, module_id)
@@ -58,7 +58,7 @@ CREATE TABLE programs_modules (
 -- таблица группы
 CREATE TABLE teaching_groups (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    slack_group VARCHAR(255),
+    slug VARCHAR(255),
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
@@ -67,13 +67,14 @@ CREATE TABLE teaching_groups (
 -- таблица пользователей
 CREATE TABLE users (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name_user VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(255) NOT NULL,
-    password_user VARCHAR(100) NOT NULL,
+    password_hash VARCHAR(100) NOT NULL,
     teaching_group_id BIGINT REFERENCES teaching_groups (id),
-    role_id VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
+    updated_at TIMESTAMP NOT NULL,
+    deleted_at SMALLINT
 );
 
 CREATE TYPE status_enrollment AS ENUM ('active', 'pending', 'cancelled', 'completed');
@@ -92,9 +93,9 @@ CREATE TYPE payment_status AS ENUM ('pending', 'paid', 'failed', 'refunded');
 CREATE TABLE payments (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     enrollment_id BIGINT REFERENCES enrollments (id) NOT NULL,
-    payment_amount DECIMAL(12, 2) NOT NULL,
+    amount DECIMAL(12, 2) NOT NULL,
     status payment_status NOT NULL,
-    date_payment TIMESTAMP NOT NULL,
+    paid_at TIMESTAMP NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
@@ -106,8 +107,8 @@ CREATE TABLE program_completions (
     user_id BIGINT REFERENCES users (id) NOT NULL,
     program_id BIGINT REFERENCES programs (id) NOT NULL,
     status status_completion NOT NULL,
-    start_program TIMESTAMP NOT NULL,
-    end_program TIMESTAMP NOT NULL,
+    started_atTIMESTAMP NOT NULL,
+    completed_at TIMESTAMP NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
@@ -117,8 +118,8 @@ CREATE TABLE certificates (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id BIGINT REFERENCES users (id),
     program_id BIGINT REFERENCES programs (id) NOT NULL,
-    url_certificate VARCHAR(255) NOT NULL,
-    date_certificate TIMESTAMP NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    issued_at TIMESTAMP NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
@@ -127,8 +128,8 @@ CREATE TABLE certificates (
 CREATE TABLE quizzes (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     lesson_id BIGINT REFERENCES lessons (id) NOT NULL,
-    name_quizze VARCHAR(255) NOT NULL,
-    content_quizze TEXT,
+    name VARCHAR(255) NOT NULL,
+    content TEXT,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
@@ -137,8 +138,8 @@ CREATE TABLE quizzes (
 CREATE TABLE exercises (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     lesson_id BIGINT REFERENCES lessons (id) NOT NULL,
-    name_exercise VARCHAR(255) NOT NULL,
-    url_exercise VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
@@ -147,7 +148,8 @@ CREATE TABLE exercises (
 CREATE TABLE discussions (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     lesson_id BIGINT REFERENCES lessons (id) NOT NULL,
-    content_discussion TEXT,
+    user_id BIGINT REFERENCES users (id) NOT NULL,
+    text TEXT,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
@@ -157,8 +159,8 @@ CREATE TYPE status_blog AS ENUM ('created', 'in moderation', 'published', 'archi
 CREATE TABLE blogs (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id BIGINT REFERENCES users (id),
-    header_blog VARCHAR(255) NOT NULL,
-    content_blog TEXT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
     status status_blog NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
